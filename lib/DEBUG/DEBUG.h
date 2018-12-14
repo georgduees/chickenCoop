d692b44949154269684cfdc76688eac32867d4a7
Georg Felix Dues <georg.duees@gmail.com>
2018-12-14 20:34:55 +0100

    initial commit

---
+++ lib/DEBUG/DEBUG.h
@@ -0,0 +1,21 @@
+/*
+ * @Author: georg.duees 
+ * @Date: 2018-12-14 20:32:20 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-12-14 20:32:57
+ */
+
+
+#ifdef DEBUG
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define DEBUG_PRINTLN(x) Serial.println(x)
+#define DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define DEBUG_FLUSH() Serial.flush()
+#else
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINT2(x, y)
+#define DEBUG_PRINTLN(x)
+#define DEBUG_PRINTLN2(x, y)
+#define DEBUG_FLUSH()
+#endif
