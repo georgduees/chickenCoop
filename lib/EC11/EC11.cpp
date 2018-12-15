aa3df1640cc8c287d3c49dbf154e2608d9bb0ab7
Georg Felix Dues <georg.duees@gmail.com>
2018-12-15 14:39:01 +0100

    added ENUM into class definition

--- lib/EC11/EC11.cpp
+++ lib/EC11/EC11.cpp
@@ -2,7 +2,7 @@
  * @Author: georg.duees 
  * @Date: 2018-12-15 12:58:46 
  * @Last Modified by: georg.duees
- * @Last Modified time: 2018-12-15 13:13:34
+ * @Last Modified time: 2018-12-15 14:38:43
  */
 #include "EC11.h"
 //#define DEBUG
@@ -19,7 +19,7 @@ EC11::EC11(byte pinA,byte pinB,byte buttonPin){
 pinMode(_pinB,INPUT);
 pinMode(_buttonPin,INPUT);
 }
-EC11_STATE EC11::value(){
+EC11::EC11_STATE EC11::value(){
     EC11_STATE retVal =left;
 
  return retVal;
--- lib/EC11/EC11.h
+++ lib/EC11/EC11.h
@@ -2,7 +2,7 @@
  * @Author: georg.duees 
  * @Date: 2018-12-15 11:56:25 
  * @Last Modified by: georg.duees
- * @Last Modified time: 2018-12-15 14:08:58
+ * @Last Modified time: 2018-12-15 14:37:29
  * Inspired by Christian Aschoff / caschoff _AT_ mac _DOT_ com Qlockthree
  */
 #ifndef EC11_H
@@ -27,24 +27,26 @@
 #define DOUBLECLICKTIME  600  // second click within 600ms
 #define HOLDTIME 1200 // report held button after 1.2s
 
-  enum EC11_STATE {
-  unknown,
-  left,
-  right,
-  click,
-  doubleclick,
-  hold
-};
+  
 class EC11 {
 public:
-  EC11(byte pinA,byte pinB,byte buttonPin);
-  EC11_STATE value();
-
+    typedef enum  EC11_STATE_e {
+        unknown=0,
+        left,
+        right,
+        click,
+        doubleclick,
+        hold
+    } EC11_STATE;
+public:
+    EC11(byte pinA,byte pinB,byte buttonPin);
+public:
+    EC11_STATE value(void);
 private:
-  byte _pinA;
-  byte _pinB;
-  byte _buttonPin;
-  int _debounceTime;
+    byte _pinA;
+    byte _pinB;
+    byte _buttonPin;
+    int _debounceTime;
 };
 
 #endif
